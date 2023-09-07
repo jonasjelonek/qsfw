e324926d047c844bf8f84cd215e626979f72836d
Jonas Jelonek <jonas.jelonek@proton.me>
2023-09-07 09:36:45 +0200

    add more tests
    Signed-off-by: Jonas Jelonek <jonas.jelonek@proton.me>

--- examples/test.py
+++ examples/test.py
@@ -1,23 +1,101 @@
 from qsfw.circuitry.quantum_state import QuantumState
 import qsfw.circuitry.quantum_gate as qgt
 
-z = QuantumState((0, 1))
+print("================ TEST 1 ================")
+z = QuantumState((0, 0, 0, 1))
 hgate = qgt.HGate()
-#cnot = qgt.CNOTGate()
 
 print("Startzustand:")
 z.print()
 
-print("Erstes Mal Hadamard auf Qubit 0!")
-z.apply_gate(hgate, ('0',))
-#print("CNOT auf qubits '0' und '1', mit '1' als C-Qubit!")
-#z.apply_gate(cnot, ('1', '0'))
+print("Erstes Mal Hadamard auf Qubit 1!")
+z.apply_gate(hgate, ('1',))
 
 print("Aktueller Zustand:")
 z.print()
 
-print("Zweites Mal Hadamard auf Qubit 0!")
-z.apply_gate(hgate, ('0',))
+print("Zweites Mal Hadamard auf Qubit 1!")
+z.apply_gate(hgate, ('1',))
+
+print("Endzustand:")
+z.print()
+
+print("\n================ TEST 2 ================")
+z = QuantumState((0, 1, 0, 1))
+hgate = qgt.HGate()
+
+print("Startzustand:")
+z.print()
+
+print("Erstes Mal Hadamard auf Qubit 1!")
+z.apply_gate(hgate, ('1',))
+
+print("Aktueller Zustand:")
+z.print()
+
+print("Zweites Mal Hadamard auf Qubit 1!")
+z.apply_gate(hgate, ('1',))
+
+print("Endzustand:")
+z.print()
+
+print("\n================ TEST 3 ================")
+
+
+
+print("\n================ TEST 4 ================")
+z = QuantumState((0, 1, 0, 1))
+cnot = qgt.CNOTGate()
+
+print("Startzustand:")
+z.print()
+
+print("CNOT auf Qubits 1 und 2, mit '1' als C-Qubit!")
+z.apply_gate(cnot, ('1', '2'))
+
+print("Aktueller Zustand:")
+z.print()
+
+print("Zweites Mal CNOT auf Qubit 1 und 2!")
+z.apply_gate(cnot, ('1', '2'))
+
+print("Endzustand:")
+z.print()
+
+print("\n================ TEST 5 ================")
+z = QuantumState((0, 1, 0, 1))
+cnot = qgt.CNOTGate()
+
+print("Startzustand:")
+z.print()
+
+print("CNOT auf Qubits 1 und 2, mit '2' als C-Qubit!")
+z.apply_gate(cnot, ('2', '1'))
+
+print("Aktueller Zustand:")
+z.print()
+
+print("Zweites Mal CNOT auf Qubit 1 und 2!")
+z.apply_gate(cnot, ('2', '1'))
+
+print("Endzustand:")
+z.print()
+
+print("\n================ TEST 5 ================")
+z = QuantumState((0, 1, 0, 1))
+ccnot = qgt.ToffoliGate()
+
+print("Startzustand:")
+z.print()
+
+print("CCNOT auf Qubits 0, 1, 3, mit 1 und 3 als C-Qubits!")
+z.apply_gate(ccnot, ('1', '3', '0'))
+
+print("Aktueller Zustand:")
+z.print()
+
+print("Zweites Mal CCNOT auf Qubit 0, 1 und 3!")
+z.apply_gate(ccnot, ('1', '3', '0'))
 
 print("Endzustand:")
 z.print()
