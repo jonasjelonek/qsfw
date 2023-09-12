fix incorrect gate initialisation causing sporadic calculation errors
Before, the gate classes Q1Gate, Q2Gate and Q3Gate initialised their
internal matrix with numpy.ndarray(). Under the incorrect assumption
that this array is zero-initialized, following initialisation methods
only set the 1s in the matrix. The final matrix mostly had any values
but the expected 0. Depending on these values, this sporadically lead to
calculation errors or even overflow in the matrix multiplication.

Fix this by initialising the matrix with numpy.zeroes() instead.

Signed-off-by: Jonas Jelonek <jonas.jelonek@proton.me>

qsfw/circuitry/quantum_gate.py
@@ -16,21 +16,21 @@ class QGate:
 
 class Q1Gate(QGate):
 	def __init__(self):
-		self.matrix = np.ndarray(shape = (2,2), dtype = complex)
+		self.matrix = np.zeros(shape = (2,2), dtype = complex)
 
 	def targeted_qubits(self) -> int:
 		return 1
 
 class Q2Gate(QGate):
 	def __init__(self):
-		self.matrix = np.ndarray(shape = (4,4), dtype = complex)
+		self.matrix = np.zeros(shape = (4,4), dtype = complex)
 
 	def targeted_qubits(self) -> int:
 		return 2
 
 class Q3Gate(QGate):
 	def __init__(self):
-		self.matrix = np.ndarray(shape = (8,8), dtype = complex)
+		self.matrix = np.zeros(shape = (8,8), dtype = complex)
 
 	def targeted_qubits(self) -> int:
 		return 3
@@ -136,13 +136,13 @@ class CPhaseGate(Q2Gate):
 class ToffoliGate(Q3Gate):
 	def __init__(self):
 		Q3Gate.__init__(self)
-		self.matrix[0,0] = self.matrix[1,1] = self.matrix[2,2] = \
-			self.matrix[3,3] = self.matrix[4,4] = self.matrix[5,5] = \
-			self.matrix[6,7] = self.matrix[7,6] = 1
+		self.matrix[0,0] = self.matrix[1,1] = self.matrix[2,2] = 1
+		self.matrix[3,3] = self.matrix[4,4] = self.matrix[5,5] = 1
+		self.matrix[6,7] = self.matrix[7,6] = 1
 		
 class CSwapGate(Q3Gate): # Controlled Swap
 	def __init__(self):
 		Q3Gate.__init__(self)
-		self.matrix[0,0] = self.matrix[1,1] = self.matrix[2,2] = \
-			self.matrix[3,3] = self.matrix[4,4] = self.matrix[5,6] = \
-			self.matrix[6,5] = self.matrix[7,7] = 1
+		self.matrix[0,0] = self.matrix[1,1] = self.matrix[2,2] = 1
+		self.matrix[3,3] = self.matrix[4,4] = self.matrix[5,6] = 1
+		self.matrix[6,5] = self.matrix[7,7] = 1

qsfw/circuitry/quantum_state.py
@@ -21,7 +21,8 @@ class QuantumState():
 	# 				i.e. |0000...0>
 	#					  ^ (index 0)
 
-	def __init__(self, values: tuple[int]|dict[str, int|str]):
+	#def __init__(self, values: tuple[int]|dict[str, int|str]):
+	def __init__(self, values):
 		self.qubits_id = dict()
 		"""
 		Keeps a mapping from qubit IDs to indices within a state tuple. Users
@@ -122,6 +123,7 @@ class QuantumState():
 				comp_vec.append(self.components[e])
 				already_processed.append(e)
 
+			comp_vec = np.array(comp_vec)
 			# Matrix of the gate adjusts the shares by simply matrix multiplication
 			result = gate.matrix @ comp_vec		# operator for matrix multiplication (Python 3.5+)
 
@@ -159,7 +161,7 @@ class QuantumState():
 				self.components[c_key] = 1.0+0j
 
 			# Remove all entries that have a share of ~0.0
-			#if self.components[c_key] == 0.0+0j:
+			# if self.components[c_key] == 0.0+0j:
 			#	del self.components[c_key]
 
 		self.components = dict(sorted(self.components.items()))